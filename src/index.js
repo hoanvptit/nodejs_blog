97c4afd161640b148f5108dddeccb6ed5c40b96e
hoanv.ptit <hoanv.ptit@gmail.com>
2021-10-10 22:08:11 +0700

    install prettier, lint-staged, husky

--- src/index.js
+++ src/index.js
@@ -1,28 +1,33 @@
-const  path = require('path');
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const handlebars = require('express-handlebars');
 
 const app = express();
 const port = 3000;
-const route = require('./routes')
+const route = require('./routes');
 
 app.use(express.static(path.join(__dirname, '/public')));
- app.use(express.urlencoded({
-     extended: true
- }));
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
 
 app.use(express.json());
 //HTTP logger
-app.use(morgan("combined"));
+app.use(morgan('combined'));
 //Template engine
-app.engine('.hbs', handlebars({
-    extname:'.hbs'
-}));
+app.engine(
+    '.hbs',
+    handlebars({
+        extname: '.hbs',
+    }),
+);
 app.set('view engine', '.hbs');
-app.set('views', path.join(__dirname, 'resource/views'))
+app.set('views', path.join(__dirname, 'resource/views'));
+
 //Route initial
 route(app);
 
-
-app.listen(port, () => console.log(" anh hoa dep trai"));
+app.listen(port, () => console.log(' anh hoa dep trai'));
