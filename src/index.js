843e05cd33731192da6803222d80b090420653d8
hoanv.ptit <hoanv.ptit@gmail.com>
2021-10-10 16:56:42 +0700

    mvc model - creare route, controller

--- src/index.js
+++ src/index.js
@@ -5,9 +5,14 @@ const handlebars = require('express-handlebars');
 
 const app = express();
 const port = 3000;
+const route = require('./routes')
 
-console.log(path.join(__dirname, '/public'));
 app.use(express.static(path.join(__dirname, '/public')));
+ app.use(express.urlencoded({
+     extended: true
+ }));
+
+app.use(express.json());
 //HTTP logger
 app.use(morgan("combined"));
 //Template engine
@@ -16,11 +21,8 @@ app.engine('.hbs', handlebars({
 }));
 app.set('view engine', '.hbs');
 app.set('views', path.join(__dirname, 'resource/views'))
-app.get('/', (req, res) => {
-    res.render('home')
-})
-app.get('/news', (req, res)=>{
-    res.render("news")
-})
+//Route initial
+route(app);
+
 
 app.listen(port, () => console.log(" anh hoa dep trai"));
